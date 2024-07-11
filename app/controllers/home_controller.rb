fa6be840cccf3451c407715310e1f9e83fe76c85
Marc <info@minuitcinq.com>
2024-07-11 11:34:14 -0400

    candidates cards fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
       .limit(6)
     # .actively_looking.newest_first
 
-    pp '##################'
+    pp "##################"
     pp @developers.count
-    pp '##################'
+    pp "##################"
     Analytics::Event.home_page_viewed(current_user, cookies)
   end
 end
