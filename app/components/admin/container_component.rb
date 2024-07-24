5491d66c02b3658bc122b6a96e51dfc6c3893368
Marc <info@minuitcinq.com>
2024-07-24 14:30:11 -0400

    admin pages framework

--- app/components/admin/container_component.rb
+++ app/components/admin/container_component.rb
@@ -1,7 +1,7 @@
 module Admin
   class ContainerComponent < ApplicationComponent
     def call
-      tag.div content, class: "min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8"
+      tag.div content, class: "container overflow-hidden max-w-5xl pt-24 pb-0 mx-auto flex flex-col justify-center sm:px-6 lg:px-8"
     end
   end
 end
