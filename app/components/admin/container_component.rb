aadc339a2ca449548d2ad2cbeab943cb583b7e38
Marc <info@minuitcinq.com>
2024-08-21 09:34:10 -0400

    User page structure Pagination relook Cards Stretched Link Hero screen adjustments

--- app/components/admin/container_component.rb
+++ app/components/admin/container_component.rb
@@ -1,7 +1,7 @@
 module Admin
   class ContainerComponent < ApplicationComponent
     def call
-      tag.div content, class: "container overflow-hidden max-w-5xl pt-24 pb-0 mx-auto flex flex-col justify-center sm:px-6 lg:px-8"
+      tag.div content, class: "container overflow-hidden pt-24 pb-0 mx-auto flex flex-col justify-center"
     end
   end
 end
